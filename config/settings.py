13d019a588fbddd834d596219138b8486975e8e0
fukushima minami <fukushimaminami1@MacBook-Air.local>
2026-04-02 18:54:20 +0900

    fix allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-&z)e3#)7ovro!f%o@zy*m%*5uryxy-a41m&!b%xnz7r+5+r2n7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'fukushimaminami.pythonanywhere.com',
+    '127.0.0.1',
+    'localhost',
+]
 
 
 # Application definition
