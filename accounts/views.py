db05c11e804d9240bece9dc74ba5dab0d11252cd
fukushima minami <fukushimaminami1@MacBook-Air.local>
2026-04-02 18:47:51 +0900

    login and signup fix

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 
 def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'accounts/login.html', {
+                'error': 'ユーザー名またはパスワードが違います'
+            })
+
     return render(request, 'accounts/login.html')
+
 
 def signup_view(request):
     if request.method == 'POST':
