923166361852f83b725c92e4995938272ffb1f4d
fukushima minami <fukushimaminami1@MacBook-Air.local>
2026-04-07 15:40:57 +0900

    change top to product search

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,14 +1,9 @@
 from django.urls import path
 from . import views
-from django.http import HttpResponse
-
-
-def home_view(request):
-    return HttpResponse("ログイン成功！")
-
 
 urlpatterns = [
-    path("", views.login_view, name="login"),
+    path("", views.product_search, name="product_search"),  # ←トップを検索に
+    path("login/", views.login_view, name="login"),
     path("signup/", views.signup_view, name="signup"),
     path("home/", views.home_view, name="home"),
     path("products/search/", views.product_search, name="product_search"),
