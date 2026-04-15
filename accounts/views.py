aac7df018367db76425778f1fcb6ea1f2419c419
fukushima minami <fukushimaminami1@MacBook-Air.local>
2026-04-16 00:51:14 +0900

    clean product search form

--- accounts/views.py
+++ accounts/views.py
@@ -321,7 +321,7 @@ def product_create(request):
             product = form.save(commit=False)
 
             product.save()
-            return redirect("product_search")
+            return redirect("product_detail", product_id=product.id)
     else:
         form = ProductForm()
 
