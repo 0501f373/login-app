88e43277b698ed10db5383f426100b84be03c5c1
fukushima minami <fukushimaminami1@MacBook-Air.local>
2026-04-29 16:54:38 +0900

    画像アップロードを任意入力に修正

--- accounts/forms.py
+++ accounts/forms.py
@@ -9,15 +9,21 @@ class MultipleFileInput(forms.ClearableFileInput):
 class MultipleFileField(forms.FileField):
     def __init__(self, *args, **kwargs):
         kwargs.setdefault("widget", MultipleFileInput())
+        kwargs.setdefault("required", False)
         super().__init__(*args, **kwargs)
 
     def clean(self, data, initial=None):
         if not data:
             return []
-        if isinstance(data, (list, tuple)):
-            return [super(MultipleFileField, self).clean(d, initial) for d in data]
-        return [super().clean(data, initial)]
 
+        if isinstance(data, (list, tuple)):
+            return [
+                super(MultipleFileField, self).clean(d, initial)
+                for d in data
+                if d
+            ]
+
+        return [super().clean(data, initial)]
 
 class ProductForm(forms.ModelForm):
     images = MultipleFileField(
